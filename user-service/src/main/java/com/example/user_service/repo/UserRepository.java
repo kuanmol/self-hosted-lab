4a9b509aaf96796d94eaaf414b8dd34017c79ac4
kuanmol <ramuklomna100@gmail.com>
2026-06-10 15:00:44 +0530

    new iamge tag creation

--- user-service/src/main/java/com/example/user_service/repo/UserRepository.java
+++ user-service/src/main/java/com/example/user_service/repo/UserRepository.java
@@ -3,6 +3,5 @@ package com.example.user_service.repo;
 import com.example.user_service.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface UserRepository extends JpaRepository<User, Long> {
 }
